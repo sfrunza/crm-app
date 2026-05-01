a18cffa197c4f976fc5d572e915f9a6464d07cd3
sfrunza <frunza.sergiu3@gmail.com>
2026-05-01 16:52:52 -0400

    prod default_url_options

--- api/config/environments/development.rb
+++ api/config/environments/development.rb
@@ -39,8 +39,8 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Set localhost to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "localhost", port: 3001 }
-  Rails.application.routes.default_url_options = { host: "localhost", port: 3001 }
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  Rails.application.routes.default_url_options = { host: "localhost", port: 3000, protocol: "http" }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
   config.action_mailer.smtp_settings = {
